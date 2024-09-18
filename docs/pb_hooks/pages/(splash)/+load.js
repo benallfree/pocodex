73e4f8d153bafe8000710baf4f8c9ebbcc4a3737
Ben Allfree <ben@benallfree.com>
2024-09-17 21:23:46 -0700

    wip

--- docs/pb_hooks/pages/(splash)/+load.js
+++ docs/pb_hooks/pages/(splash)/+load.js
@@ -3,110 +3,14 @@ module.exports = () => {
     favorited: {},
     plugins: [
       {
-        name: 'pages-daisyui',
-        description: 'A PocketPages starter kit using DaisyUI and TailwindCSS',
-        github_url:
-          'https://github.com/benallfree/pocodex/tree/master/plugins/pages-daisyui',
-        owner: 'benallfree',
-        version: '1.0.0',
-        stars: 120,
+        description: 'Server-side JS pages for PocketBase',
+        npm_name: 'pocketpages',
+        github_name: 'benallfree/pocketpages',
       },
       {
-        name: 'pages-daisyui-docs',
-        description:
-          'A docs site starter kit for PocketPages using DaisyUI and TailwindCSS',
-        github_url:
-          'https://github.com/benallfree/pocodex/tree/master/plugins/pages-daisyui-docs',
-        owner: 'benallfree',
-        version: '1.0.0',
-        stars: 95,
-      },
-      {
-        name: 'pages-minimal',
-        description: 'A minimal PocketPages starter kit',
-        github_url:
-          'https://github.com/benallfree/pocodex/tree/master/plugins/pages-minimal',
-        owner: 'benallfree',
-        version: '1.0.0',
-        stars: 80,
-      },
-      {
-        name: 'otp',
+        npm_name: 'pocketbase-otp',
         description: 'One Time Password API for PocketBase',
-        github_url:
-          'https://github.com/benallfree/pocodex/tree/master/plugins/otp',
-        owner: 'benallfree',
-        version: '1.0.0',
-        stars: 150,
-      },
-      {
-        name: 'pages-otp-alpinejs',
-        description: 'A PocketPages AlpineJS UI for OTP',
-        github_url:
-          'https://github.com/benallfree/pocodex/tree/master/plugins/pages-otp-alpinejs',
-        owner: 'benallfree',
-        version: '1.0.0',
-        stars: 60,
-      },
-      {
-        name: 'nodejs-otp-cli',
-        description:
-          'A Node.js CLI (commander) plugin to do OTP via command line',
-        github_url:
-          'https://github.com/benallfree/pocodex/tree/master/plugins/nodejs-otp-cli',
-        owner: 'benallfree',
-        version: '1.0.0',
-        stars: 85,
-      },
-      {
-        name: 'pages-pockethost-manual-deploy',
-        description:
-          'Deploy your PocketPages site to PocketHost.io using a manual script',
-        github_url:
-          'https://github.com/benallfree/pocodex/tree/master/plugins/pages-pockethost-manual-deploy',
-        owner: 'benallfree',
-        version: '1.0.0',
-        stars: 70,
-      },
-      {
-        name: 'pages-pockethost-ga-deploy',
-        description:
-          'Deploy your PocketPages site to PocketHost.io using a GitHub Act script',
-        github_url:
-          'https://github.com/benallfree/pocodex/tree/master/plugins/pages-pockethost-ga-deploy',
-        owner: 'benallfree',
-        version: '1.0.0',
-        stars: 75,
-      },
-      {
-        name: 'pages-fly-manual-deploy',
-        description:
-          'Deploy your PocketPages site to fly.io using a manual script',
-        github_url:
-          'https://github.com/benallfree/pocodex/tree/master/plugins/pages-fly-manual-deploy',
-        owner: 'benallfree',
-        version: '1.0.0',
-        stars: 65,
-      },
-      {
-        name: 'pages-fly-ga-deploy',
-        description:
-          'Deploy your PocketPages site to fly.io using a GitHub Act script',
-        github_url:
-          'https://github.com/benallfree/pocodex/tree/master/plugins/pages-fly-ga-deploy',
-        owner: 'benallfree',
-        version: '1.0.0',
-        stars: 90,
-      },
-      {
-        name: 'lazy-cron',
-        description:
-          'Run crons on startup if they would have run while PocketBase was not running',
-        github_url:
-          'https://github.com/benallfree/pocodex/tree/master/plugins/lazy-cron',
-        owner: 'benallfree',
-        version: '1.0.0',
-        stars: 110,
+        github_name: 'benallfree/pocketbase-otp',
       },
     ],
   }
